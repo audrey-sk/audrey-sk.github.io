d8bcab3d624a2b15ca1cfe249f0ece8fdcb515f1
Audrey Safikhani <safikhani.parasto@gmail.com>
2025-12-28 17:32:29 -0800

    cleaned up deployment proprocess

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    // This allows process.env to work in the browser for the API key
-    'process.env': process.env
-  }
+  build: {
+    outDir: 'dist', // default, but explicit is fine
+  },
 });
